0e0630c6edea6b2c42933a3934a75e5f90818932
khangyessir <tranphuckhang13102008@gmail.com>
2024-06-22 10:40:57 +0700

    first commit

---
+++ numberOfSubarrays.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int numberOfSubarrays(vector<int>& nums, int k){
+        int n = nums.size();
+        int count = 0;
+        int sum = 0;
+        unordered_map<int, int> prefix;
+        prefix[0] = 1;
+        for(int i=0; i<n; ++i){
+            sum += (nums[i] % 2 == 1 ? 1 : 0);
+            if(sum >= k){
+                count += prefix[sum - k];
+            }
+            ++prefix[sum];
+        }
+        return count;
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums = {2,2,2,1,2,2,1,2,2,2};
+    int k = 2;
+    cout << sol.numberOfSubarrays(nums, k) << endl;
+    return 0;
+}
+
